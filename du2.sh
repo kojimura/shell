39a97c54358190501b5f0e596502ae9b32b86f17
kojimura <33794193+kojimura@users.noreply.github.com>
2025-06-15 17:43:22 +0900

    Create du2.sh

---
+++ du2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Check the disk usage, show the big old files
+
+DAYS=1825
+SIZE="100M"
+PTH="/"
+
+while getopts ":d:s:p:" opt; do
+    case $opt in
+        d)  DAYS=$OPTARG
+            echo "-d $OPTARG"
+            ;;
+        s)  SIZE=$OPTARG
+            echo "-s $OPTARG"
+            ;;
+        p)  PTH=$OPTARG
+            echo "-p $OPTARG"
+            ;;
+        \?) echo "Invalid $OPTARG Usage: $0 [-p path][-d days][-s size]"
+            exit 1
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+echo "Finding big(>${SIZE} old(>${DAYS}) files in $PTH >>>"
+sudo find ${dir} -type f mtime +${DAYS} -size +${SIZE} -exec ls -lh {} \;
